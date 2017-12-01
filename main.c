88247e50d06ea00965b5f2d942c2645bb620b673
Basem M. Gaber <bmgaberah@hotmail.com>
2017-12-01 19:52:33 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+typedef struct {
+    int day, month, year;
+} Date;
+
 typedef struct {
 char Book_Title[100];
 char Author[50];
 char publisher[50];
 char ISBN[18]; // International Standard Book Number ..Example: 978-3-16-148410-0
-char DateOfPuplication[11]; // Example 30/11/2017
+Date DateOfPuplication[11]; // Example 30/11/2017
 int number_of_copies ;
 int current_available_number_of_copies;
 char category[30];
@@ -25,9 +30,7 @@ char member_Phone_Number[12];
 char member_age[4];
 char    member_Email[30];
 }member;
-typedef struct {
-    int day, month, year;
-} Date;
+
 typedef struct{
     char borrowed_ISBN[18];
     char user_i[9];
